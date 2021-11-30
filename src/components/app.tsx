bfcedac949e5bf6e9fc07c1c37d2f34c11ed1e4b
Adrien Pyke <adrien.pyke@shopify.com>
2021-11-30 15:00:28 -0500

    first commit

---
+++ src/components/app.tsx
@@ -0,0 +1,21 @@
+import { FunctionalComponent, h } from 'preact'
+import { Route, Router } from 'preact-router'
+
+import Home from '../routes/home'
+import Profile from '../routes/profile'
+import NotFoundPage from '../routes/notfound'
+import Header from './header'
+
+const App: FunctionalComponent = () => (
+  <div id="preact_root">
+    <Header />
+    <Router>
+      <Route path="/" component={Home} />
+      <Route path="/profile/" component={Profile} user="me" />
+      <Route path="/profile/:user" component={Profile} />
+      <NotFoundPage default />
+    </Router>
+  </div>
+)
+
+export default App
