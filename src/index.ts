7270aa4d4cbc84212971c15f9699f683098e42d2
Adrien Pyke <adrien.pyke@shopify.com>
2021-12-01 16:14:37 -0500

    add sanitize.css

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+import 'sanitize.css'
+import 'sanitize.css/forms.css'
+import 'sanitize.css/assets.css'
+import 'sanitize.css/typography.css'
+import 'sanitize.css/reduce-motion.css'
 import 'prism-themes/themes/prism-xonokai'
 import './style/index.css'
 import { App } from './components/App'
