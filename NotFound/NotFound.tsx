34e29e58b4a5518ab25f702ce10fa8481f485626
Adrien Pyke <adrien.pyke@shopify.com>
2021-12-01 01:29:47 -0500

    fix build

--- NotFound/NotFound.tsx
+++ NotFound/NotFound.tsx
@@ -1,10 +1,11 @@
 import { FunctionalComponent, h } from 'preact'
 import { Link } from 'preact-router/match'
+import { baseRoute } from '../../baseRoute'
 
 export const NotFound: FunctionalComponent = () => (
   <div>
     <h1>Error 404</h1>
     <p>That page doesn&apos;t exist.</p>
-    <Link href="/">Back to Home</Link>
+    <Link href={`${baseRoute}/`}>Back to Home</Link>
   </div>
 )
