eb143ca1d660ae28c5e682dbc5d369645ea289e8
Adrien Pyke <adrien.pyke@shopify.com>
2021-12-03 02:06:41 -0500

    add link to github

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,27 +1,34 @@
 import { h } from 'preact'
 import { Icon } from '/components'
 import { setNavOpen, useStore } from '/store'
+import { newTab } from '/shared/web-utilities/util'
 import style from './style.css'
 
 export const Header = () => {
   const { navOpen } = useStore()
   return (
     <header class={style.header}>
+      <button
+        class={style.navBtn}
+        onClick={(e) => {
+          setNavOpen(!navOpen)
+          e.stopImmediatePropagation()
+        }}
+        aria-label="open navigation menu"
+      >
+        <Icon name="menu" />
+      </button>
       <a class={style.logo} href="#/">
         advent of code 2021
       </a>
-      <div class={style.nav}>
-        <button
-          class={style.navBtn}
-          onClick={(e) => {
-            setNavOpen(!navOpen)
-            e.stopImmediatePropagation()
-          }}
-          aria-label="open navigation menu"
-        >
-          <Icon name="menu" />
-        </button>
-      </div>
+      <a
+        class={style.navBtn}
+        href="https://github.com/kufii/advent-of-code-2021"
+        aria-label="open github repository"
+        {...newTab}
+      >
+        <Icon name="github" />
+      </a>
     </header>
   )
 }
