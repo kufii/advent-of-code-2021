74b36e58a69cd88df5a36aa1ea2770c4af7efc00
Adrien Pyke <adrien.pyke@shopify.com>
2021-12-09 11:06:09 -0500

    add button to show/hide the visualization

--- src/components/Visualization/Visualization.tsx
+++ src/components/Visualization/Visualization.tsx
@@ -1,12 +1,27 @@
-import { ComponentChild, h } from 'preact'
-import style from './style.css'
+import { ComponentChild, h, Fragment } from 'preact'
+import { useState } from 'preact/hooks'
 import { classNames } from '/shared/web-utilities/util'
+import style from './style.css'
+import { Button, Icon } from '..'
 
 interface Props {
   class?: string
   children: ComponentChild
 }
 
-export const Visualization = ({ class: className, children }: Props) => (
-  <pre class={classNames(style.container, className)}>{children}</pre>
-)
+export const Visualization = ({ class: className, children }: Props) => {
+  const [show, setShow] = useState(false)
+  return (
+    <>
+      <div>
+        <Button onClick={() => setShow(!show)} plain compact>
+          <Icon name={show ? 'eye-off' : 'eye'} />
+          {show ? 'Hide' : 'Show'} visualization
+        </Button>
+      </div>
+      {show && (
+        <pre class={classNames(style.container, className)}>{children}</pre>
+      )}
+    </>
+  )
+}
