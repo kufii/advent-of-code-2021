58536576ad2f42b5221146e0ea4b4cf3babb8af5
Adrien Pyke <adrien.pyke@shopify.com>
2021-12-21 23:37:16 -0500

    minor refactor

--- src/routes/NotFound/NotFound.tsx
+++ src/routes/NotFound/NotFound.tsx
@@ -5,7 +5,7 @@ import style from './style.css'
 export const NotFound: FunctionalComponent = () => (
   <div class={style.container}>
     <h1>Error 404</h1>
-    <p>That page doesn&apos;t exist.</p>
+    <p>{"That page doesn't exist."}</p>
     <div>
       <Button url="#" plain compact>
         Return to home page
