1e2c30e361911500320409861680929d6676e35f
Adrien Pyke <adrien.pyke@shopify.com>
2021-12-21 23:36:11 -0500

    improve styles on 404 page

--- src/routes/NotFound/NotFound.tsx
+++ src/routes/NotFound/NotFound.tsx
@@ -1,10 +1,15 @@
 import { FunctionalComponent, h } from 'preact'
+import { Button } from '/components'
 import style from './style.css'
 
 export const NotFound: FunctionalComponent = () => (
   <div class={style.container}>
     <h1>Error 404</h1>
     <p>That page doesn&apos;t exist.</p>
-    <a href="#">Back to Home</a>
+    <div>
+      <Button url="#" plain compact>
+        Return to home page
+      </Button>
+    </div>
   </div>
 )
